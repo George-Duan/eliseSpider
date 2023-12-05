42155e527c598fed5d474cfb56c32ef83c975aa5
dlixiong <george.duanshier@gmail.com>
2023-12-05 20:22:53 +0800

    init

---
+++ eliseSpider/spiders/douban.py
@@ -0,0 +1,16 @@
+import scrapy
+from eliseSpider.items import ElisespiderItem
+
+
+class DoubanSpider(scrapy.Spider):
+    name = "douban"
+    allowed_domains = ["book.douban.com"]
+    start_urls = ["https://www.douban.com/tag/%E5%B0%8F%E8%AF%B4/book?start=0"]
+
+    def parse(self, response):
+        print("===============================================================")
+        item = ElisespiderItem()
+        book_list = response.xpath('/dl')
+        for book in book_list:
+            item['book_icon_img'] = book.xpath('/dt/a/img//@src').text()
+            item['detail_link'] = book.xpath('/dt/a//@href').text()
